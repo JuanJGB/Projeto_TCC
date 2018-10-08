18e8c9679d2d3b9b303cbf939989b8227e3caa0f
JuanJGB <juanbenini123@gmail.com>
2018-10-08 11:18:48 -0300

    Projeto em andamento. Fazendo páginas de login e cadastro.

--- operacoes.php
+++ operacoes.php
@@ -73,6 +73,10 @@
             <a class="nav-link" href="register.php">
                 <span>Cadastro</span></a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link" href="sobre.php">
+                <span>Sobre</span></a>
+        </li>
     </ul>
 
     <div id="content-wrapper">
@@ -80,9 +84,58 @@
         <div class="container-fluid">
 
             <!-- Page Content -->
-            <h1>Index Page</h1>
+
+            <h1>Cadastro de Operações:</h1>
             <hr>
             <p>This is a great starting point for new custom pages.</p>
+            <div class="container">
+                <div class="card card-register mx-auto mt-5">
+                    <div class="card-header">Cadastro</div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <div class="form-label-group">
+                                            <input type="text" id="firstName" class="form-control" placeholder="ID Operação" required="required" autofocus="autofocus">
+                                            <label for="firstName">ID Operação</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-label-group">
+                                            <input type="text" id="lastName" class="form-control" placeholder="Last name" required="required">
+                                            <label for="lastName">Last name</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="form-label-group">
+                                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required">
+                                    <label for="inputEmail">Email address</label>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="form-row">
+                                    <div class="col-md-6">
+                                        <div class="form-label-group">
+                                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
+                                            <label for="inputPassword">Password</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-label-group">
+                                            <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
+                                            <label for="confirmPassword">Confirm password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a class="btn btn-primary btn-block" href="login.php">Register</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         </div>
         <!-- /.container-fluid -->
