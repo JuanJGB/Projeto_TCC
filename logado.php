499a850fa0f6111bdd3eba2db534561ebce3031d
JuanJGB <juanbenini123@gmail.com>
2018-10-19 21:11:13 -0300

    Continuando páginas de login e cadastro.

--- logado.php
+++ logado.php
@@ -32,17 +32,19 @@ require_once "conexao.php";
 <body id="page-top" >
 
 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+
+    <a class="navbar-brand mr-1" href="logado.php">Página Inicial</a>
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
     </button>
 
-    <a class="navbar-brand mr-1" href="logado.php">Página Inicial</a>
 
 
-
-    <!-- Navbar Search -->
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+
     </form>
+
 
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
