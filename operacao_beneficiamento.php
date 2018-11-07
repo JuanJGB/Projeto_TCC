3034b62fddc587301f02921289b92d95a01136f0
JuanJGB <juanbenini123@gmail.com>
2018-11-07 16:47:24 -0200

    Alterado a barra de navegação.

--- operacao_beneficiamento.php
+++ operacao_beneficiamento.php
@@ -33,12 +33,7 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark sticky-top ">
 
 
-
-
     <a class="navbar-brand mr-1" href="logado.php">DyOpe Assistant</a>
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-        <i class="fas fa-bars"></i>
-    </button>
 
 
     <!-- Navbar Search -->
--- operacao_costura.php
+++ operacao_costura.php
@@ -34,10 +34,6 @@
 
 
     <a class="navbar-brand mr-1" href="logado.php">DyOpe Assistant</a>
-    <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
-        <i class="fas fa-bars"></i>
-    </button>
-
 
 
     <!-- Navbar Search -->
